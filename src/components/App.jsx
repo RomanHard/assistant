062d1d144c604ae3155aa13c06acbc09d0f3a6be
Roman <romeenice@gmail.com>
2023-05-16 18:33:02 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+import React from 'react';
+import './App.css';
+
 export const App = () => {
+  const handleMenuToggle = () => {
+  
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="app">
+      <header className="header">
+        <h1 className="project-title">Assistant</h1>
+      </header>
+      <nav className="dropdown-menu">
+        {/* Додайте блоки випадаючого списку */}
+      </nav>
+      <div className="menu-toggle" onClick={handleMenuToggle}>
+        <div className="menu-icon"></div>
+        <div className="menu-icon"></div>
+        <div className="menu-icon"></div>
+      </div>
     </div>
   );
 };
+
