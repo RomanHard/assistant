b38a3e7487efd75d36ecd38215554f27d1280200
Roman <romeenice@gmail.com>
2023-05-16 18:58:30 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,51 +4,47 @@ import Expenses from './expenses/Expenses';
 import Notes from './notes/Notes';
 import Tasks from './tasks/Tasks';
 import Settings from './settings/Settings';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 export const App = () => {
   const handleMenuToggle = () => {
-  
+    // handle menu toggle logic here
   };
 
   return (
     <BrowserRouter>
-    <div className="app">
-      <header className="header">
-        <h1 className="project-title">Assistant</h1>
-      </header>
-      <nav className="dropdown-menu">
-        {<ul className="nav-list">
-          <li className="nav-list-item">
-            <a href="#" className="nav-link">
-<Expenses />
-              </a>
-              </li>
-              <li className="nav-list-item">
-                <a href="#" className="nav-link">
-<Notes />
-                  </a>
-                  </li>
-                      <li className="nav-list-item">
-                        <a href="#" className="nav-link">
-<Tasks />
-                          </a>
-                          </li>
-                                  <li className="nav-list-item">
-                                    <a href="#" className="nav-link">
-<Settings />
-                                      </a>
-                                      </li>
-                                      </ul>}
-
-      </nav>
-      <div className="menu-toggle" onClick={handleMenuToggle}>
-        <div className="menu-icon"></div>
-        <div className="menu-icon"></div>
-        <div className="menu-icon"></div>
+      <div className="app">
+        <header className="header">
+          <h1 className="project-title">Assistant</h1>
+        </header>
+        <nav className="dropdown-menu">
+          <ul className="nav-list">
+            <li className="nav-list-item">
+              <Link to="/expenses" className="nav-link">Cost accounting</Link>
+            </li>
+            <li className="nav-list-item">
+              <Link to="/notes" className="nav-link">Notes</Link>
+            </li>
+            <li className="nav-list-item">
+              <Link to="/tasks" className="nav-link">Tasks</Link>
+            </li>
+            <li className="nav-list-item">
+              <Link to="/settings" className="nav-link">Settings</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="menu-toggle" onClick={handleMenuToggle}>
+          <div className="menu-icon"></div>
+          <div className="menu-icon"></div>
+          <div className="menu-icon"></div>
+        </div>
+        <Routes>
+          <Route path="/expenses" element={<Expenses />} />
+          <Route path="/notes" element={<Notes />} />
+          <Route path="/tasks" element={<Tasks />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
       </div>
-    </div>
     </BrowserRouter>
   );
 };
-
