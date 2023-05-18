31e7b2af892061a674a73b6a08e82439e8ae7165
Roman <romeenice@gmail.com>
2023-05-18 15:22:04 +0300

    leftBlock

--- src/components/expenses/block/LeftBlock.js
+++ src/components/expenses/block/LeftBlock.js
@@ -9,46 +9,41 @@ const LeftBlock = () => {
 
   return (
     <div className="left-block">
-      <h2>Доходи</h2>
+      <h2 className="block-title">Доходи</h2>
       <div className="income-form">
-        <div className="income-source">
-          <label htmlFor="income-source">Джерело:</label>
-          <select id="income-source">
-            <option value="source1">Джерело 1</option>
-            <option value="source2">Джерело 2</option>
-            <option value="source3">Джерело 3</option>
-          </select>
-        </div>
-        <div className="income-amount">
+        <div className="input-field">
           <label htmlFor="amount">Сума:</label>
           <input type="text" id="amount" />
         </div>
-        <div className="income-comment">
-          <label htmlFor="comment">Коментарій:</label>
-          <input type="text" id="comment" />
+        <div className="input-field">
+          <label htmlFor="category">Категорія:</label>
+          <select id="category">
+            <option value="category1">Категорія 1</option>
+            <option value="category2">Категорія 2</option>
+            <option value="category3">Категорія 3</option>
+          </select>
         </div>
-        <div className="income-categories">
-          <label>Категорії:</label>
-          <div>
-            <input type="checkbox" id="category1" />
-            <label htmlFor="category1">Категорія 1</label>
-          </div>
-          <div>
-            <input type="checkbox" id="category2" />
-            <label htmlFor="category2">Категорія 2</label>
-          </div>
-          <div>
-            <input type="checkbox" id="category3" />
-            <label htmlFor="category3">Категорія 3</label>
-          </div>
-          <button onClick={handleEditCategories}>Редагувати категорії доходу</button>
+        <div className="input-field">
+          <label htmlFor="wallet">Гаманець:</label>
+          <select id="wallet">
+            <option value="wallet1">Гаманець 1</option>
+            <option value="wallet2">Гаманець 2</option>
+            <option value="wallet3">Гаманець 3</option>
+          </select>
+        </div>
+        <div className="input-field">
+          <label htmlFor="date">Дата:</label>
+          <input type="date" id="date" defaultValue={new Date().toISOString().split('T')[0]} />
+        </div>
+        <div className="button-row">
+          <button type="submit">Додати</button>
+          <button type="reset">Скинути</button>
         </div>
         {showModal && (
           <div className="modal" style={{ backgroundColor: '#588157' }}>
             {/* Вміст модального вікна */}
           </div>
         )}
-        <button>Додати</button>
       </div>
     </div>
   );
