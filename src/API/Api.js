1859870dafe9cd29013f59738cfa712eb876e190
Roman <romeenice@gmail.com>
2023-05-27 16:57:04 +0300

    fix app

--- src/API/Api.js
+++ src/API/Api.js
@@ -16,51 +16,69 @@ const client = new MongoClient(uri, {
   }
 });
 
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
+client.connect().then(() => {
+  console.log("Connected to MongoDB...");
+
+  app.use(function(req, res, next) {
+      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+      res.setHeader('Access-Control-Allow-Credentials', true);
+      next();
+  });
+
+  app.use(cors());
+  app.post('/saveData', async (req, res) => {
+    try {
+      const database = client.db('assistant');
+      const collection = database.collection('zinger');
+
+      const dataToSave = req.body;
+      const result = await collection.insertOne(dataToSave);
+
+      res.status(200).json({ message: `Data was saved with id: ${result.insertedId}` });
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('An error occurred while trying to save data');
+    }
+  });
+
+  const PORT = process.env.PORT || 1727;
+  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+  app.get('/getData', async (req, res) => {
+      try {
+        const database = client.db('assistant');
+        const collection = database.collection('zinger');
+    
+        const data = await collection.find().toArray();
+    
+        res.json(data);
+      } catch (err) {
+        console.error(err);
+        res.status(500).send('An error occurred while retrieving data');
+      }
+  });
+}).catch(err => {
+  console.log("Error connecting to MongoDB: ", err);
 });
 
-app.use(cors());
-app.post('/saveData', async (req, res) => {
+app.delete('/deleteCategory/:id', async (req, res) => {
   try {
-    await client.connect();
-
+    const { id } = req.params;
     const database = client.db('assistant');
     const collection = database.collection('zinger');
 
-    const dataToSave = req.body;
-    const result = await collection.insertOne(dataToSave);
+    // Assuming the id is in MongoDB's ObjectID format
+    const result = await collection.deleteOne({ _id: new MongoClient.ObjectID(id) });
 
-    res.status(200).json({ message: `Data was saved with id: ${result.insertedId}` });
+    if (result.deletedCount === 1) {
+      res.status(200).json({ message: "Successfully deleted category" });
+    } else {
+      res.status(404).json({ message: "Category not found" });
+    }
   } catch (err) {
     console.error(err);
-    res.status(500).send('An error occurred while trying to save data');
-  } finally {
-    await client.close();
+    res.status(500).send('An error occurred while trying to delete category');
   }
-});
-
-const PORT = process.env.PORT || 1727;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-
-app.get('/getData', async (req, res) => {
-    try {
-      await client.connect();
-  
-      const database = client.db('assistant');
-      const collection = database.collection('zinger');
-  
-      const data = await collection.find().toArray();
-  
-      res.json(data);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('An error occurred while retrieving data');
-    } finally {
-      await client.close();
-    }
 });
